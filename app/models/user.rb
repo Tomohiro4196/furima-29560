b56d10f83fde9e85ee1530825ecc409039de452a
Tomohiro4196 <tomo4196.w@gmail.com>
2020-12-08 23:02:23 +0900

    バリデーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :nickname,    presense: true
+  validates :birthday,    presense: true
+  validates :family_name, presense: true
+  validates :first_name,  presense: true
+  validates :family_kana, presense: true
+  validates :first_kana,  presense: true
 end
