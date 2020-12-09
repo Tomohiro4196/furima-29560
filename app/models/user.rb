bd247ca2d2bb358f9129d2d8a6f62d9da290604f
Tomohiro4196 <tomo4196.w@gmail.com>
2020-12-09 20:54:57 +0900

    パスワードのバリデーションの修正
    6-70文字、小文字１つ数字１つのバリデーションを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,4 +10,11 @@ class User < ApplicationRecord
   validates :first_name,  presence: true
   validates :family_kana, presence: true
   validates :first_kana,  presence: true
+  validates :password_complexity
+
+  def password_complexity
+    return if password.blank? || password =~ /^(?=.*?[a-z])(?=.*?[0-9]).{6,70}$/
+
+    error.add :password, 'Complexity requirement not met. Length should be 8-70 characters and include: 1 uppercase, 1 lowercase, 1 digit and 1 special character'
+  end
 end
