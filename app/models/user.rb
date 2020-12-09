d3f264a96f6189a7fc2199bfcd7b7ad11908dae1
Tomohiro4196 <tomo4196.w@gmail.com>
2020-12-09 21:19:24 +0900

    バリデーションの修正（統一）

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,17 +4,21 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :nickname,    presence: true
-  validates :birthday,    presence: true
-  validates :family_name, presence: true
-  validates :first_name,  presence: true
-  validates :family_kana, presence: true
-  validates :first_kana,  presence: true
-  validates :password_complexity
+  with_options presence: true do
+    validates :nickname    
+    validates :birthday
+    validates :family_name
+    validates :first_name
+    validates :family_kana
+    validates :first_kana
+  end
+
+ 
+  validate :password_complexity
 
   def password_complexity
     return if password.blank? || password =~ /^(?=.*?[a-z])(?=.*?[0-9]).{6,70}$/
 
-    error.add :password, 'Complexity requirement not met. Length should be 8-70 characters and include: 1 uppercase, 1 lowercase, 1 digit and 1 special character'
+    errors.add :password, 'Complexity requirement not met. Length should be 8-70 characters and include: 1 uppercase, 1 lowercase, 1 digit and 1 special character'
   end
 end
