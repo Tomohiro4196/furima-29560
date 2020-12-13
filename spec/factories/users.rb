cb7998480cbcbf9575d1b5a539747a3c17f93f0e
Tomohiro4196 <tomo4196.w@gmail.com>
2020-12-14 06:49:34 +0900

    FactoryBotの記述

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -4,5 +4,10 @@ FactoryBot.define do
     email                 {Faker::Internet.free_email}
     password              {Faker::Internet.password}
     password_confirmation {password}
+    family_name           {Gimei.last.kanji}
+    first_name            {Gimei.first.kanji}
+    family_kana           {Gimei.last.katakana}
+    first_kana            {Gimei.first.katakana}
+    birthday              {Faker::Date.backward}
   end
 end
