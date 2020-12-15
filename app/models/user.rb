dbcb5cc06a4cd565a0d95f2818086429efd6ff20
Tomohiro4196 <tomo4196.w@gmail.com>
2020-12-15 17:28:06 +0900

    rubocopによる修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,33 +2,32 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable   
-         
+         :recoverable, :rememberable, :validatable
+
   katakana_reg = /\A^[ァ-ンー－]+$\z/
   zennkaku_reg = /\A[ぁ-んァ-ン一-龥]/
 
   with_options presence: true do
-    validates :nickname    
+    validates :nickname
     validates :birthday
-      with_options format: {
-        with: zennkaku_reg,
-        message: "は全角のみで入力してください"
-       } do
-        validates :family_name
-        validates :first_name
-       end
+    with_options format: {
+      with: zennkaku_reg,
+      message: 'は全角のみで入力してください'
+    } do
+      validates :family_name
+      validates :first_name
+    end
 
-      with_options format: { 
-        with:  katakana_reg,
-        message: "は全角カタカナのみで入力して下さい"
-      } do
-        validates :family_kana
-        validates :first_kana
-      end
+    with_options format: {
+      with: katakana_reg,
+      message: 'は全角カタカナのみで入力して下さい'
+    } do
+      validates :family_kana
+      validates :first_kana
+    end
   end
 
   validate :password_complexity
-
 
   def password_complexity
     return if password =~ /^(?=.*?[a-z])(?=.*?[0-9]).{1,70}$/
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,7 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  
   before do
     @user = FactoryBot.build(:user)
   end
@@ -18,11 +17,11 @@ RSpec.describe User, type: :model do
       end
 
       it 'メールアドレスに同じものがなければ登録できる' do
-       user1 = FactoryBot.build(:user)
-       user1.email = 'user1@user.com'
-       user2 = FactoryBot.build(:user)
-       user2.email = 'user2@user.com'
-       expect(user2).to be_valid
+        user1 = FactoryBot.build(:user)
+        user1.email = 'user1@user.com'
+        user2 = FactoryBot.build(:user)
+        user2.email = 'user2@user.com'
+        expect(user2).to be_valid
       end
 
       it 'passwordが6文字以上かつ英数字混合であれば登録できる' do
@@ -48,7 +47,7 @@ RSpec.describe User, type: :model do
         @user.valid?
         expect(@user.errors.full_messages).to include("Nickname can't be blank")
       end
-      
+
       it 'emailが空欄では登録できない' do
         @user.email = ''
         @user.valid?
@@ -60,31 +59,31 @@ RSpec.describe User, type: :model do
         user2 = FactoryBot.build(:user)
         user2.email = @user.email
         user2.valid?
-        expect(user2.errors.full_messages).to include("Email has already been taken")
+        expect(user2.errors.full_messages).to include('Email has already been taken')
       end
 
       it '@を含まないemailは登録できない' do
-        @user.email = "abcd123"
+        @user.email = 'abcd123'
         @user.valid?
-        expect(@user.errors.full_messages).to include("Email is invalid")
+        expect(@user.errors.full_messages).to include('Email is invalid')
       end
 
       it 'passwordが空欄では登録できない' do
-        @user.password = ""
+        @user.password = ''
         @user.valid?
         expect(@user.errors.full_messages).to include("Password can't be blank")
       end
 
-      it "passwordは6文字以上でなければならない" do
-        @user.password = "a1"
+      it 'passwordは6文字以上でなければならない' do
+        @user.password = 'a1'
         @user.valid?
-        expect(@user.errors.full_messages).to include("Password is too short (minimum is 6 characters)")
+        expect(@user.errors.full_messages).to include('Password is too short (minimum is 6 characters)')
       end
 
       it 'passwordは半角英数字混合でなければならない' do
         @user.password = '222222'
         @user.valid?
-        expect(@user.errors.full_messages).to include("Password should include 1 lowercase and 1 digit, and more than 6")
+        expect(@user.errors.full_messages).to include('Password should include 1 lowercase and 1 digit, and more than 6')
       end
 
       it 'password_confirmationが空欄では登録できない' do
@@ -94,39 +93,38 @@ RSpec.describe User, type: :model do
       end
 
       it 'ユーザー本名が苗字と名前の両方がなければ登録できない' do
-        @user.first_name = ""
+        @user.first_name = ''
         @user.valid?
-        expect(@user.errors.full_messages).to include("First name can't be blank", "First name は全角のみで入力してください")
+        expect(@user.errors.full_messages).to include("First name can't be blank", 'First name は全角のみで入力してください')
 
-        @user.family_name = ""
+        @user.family_name = ''
         @user.valid?
-        expect(@user.errors.full_messages).to include("Family name can't be blank", "Family name は全角のみで入力してください")
-      end 
+        expect(@user.errors.full_messages).to include("Family name can't be blank", 'Family name は全角のみで入力してください')
+      end
 
       it 'ユーザー本名は全角でなければならない' do
-        @user.first_name = "123"
+        @user.first_name = '123'
         @user.valid?
-        expect(@user.errors.full_messages).to include("First name は全角のみで入力してください")
+        expect(@user.errors.full_messages).to include('First name は全角のみで入力してください')
       end
 
       it 'フリガナがなければ登録できない' do
-        @user.first_kana = ""
+        @user.first_kana = ''
         @user.valid?
-        expect(@user.errors.full_messages).to include("First kana can't be blank", "First kana は全角カタカナのみで入力して下さい")
+        expect(@user.errors.full_messages).to include("First kana can't be blank", 'First kana は全角カタカナのみで入力して下さい')
       end
 
       it 'フリガナは全角カタカナでないと登録できない' do
-        @user.first_kana = "山田"
+        @user.first_kana = '山田'
         @user.valid?
-        expect(@user.errors.full_messages).to include("First kana は全角カタカナのみで入力して下さい")
+        expect(@user.errors.full_messages).to include('First kana は全角カタカナのみで入力して下さい')
       end
 
       it '生年月日がなければ登録できない' do
-        @user.birthday = ""
+        @user.birthday = ''
         @user.valid?
         expect(@user.errors.full_messages).to include("Birthday can't be blank")
       end
     end
   end
-
 end
