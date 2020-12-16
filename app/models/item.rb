8de9e76b9fc0f5981c9e1affd62689997a8e95f7
Tomohiro4196 <tomo4196.w@gmail.com>
2020-12-17 06:55:16 +0900

    ActiveHashに対してのアソシエーション

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,9 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :condition
+  belongs_to :shipping_fee
+  belongs_to :prefecture
+  belongs_to :days_ship
+  belongs_to :category
+
 end
