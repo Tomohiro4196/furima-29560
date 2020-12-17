710d58f3ccc4704648fe36f9cd410055e2412e07
Tomohiro4196 <tomo4196.w@gmail.com>
2020-12-17 12:09:54 +0900

    shipping_feeを記述

--- app/models/shipping_fee.rb
+++ app/models/shipping_fee.rb
@@ -1,2 +1,10 @@
-class ShippingFee < ApplicationRecord
+class ShippingFee < ActiveHash::Base
+  self.data = [
+    {id: 1, paid_by: '---'},
+    {id: 2, paid_by: '着払い（購入者負担）'},
+    {id: 3, paid_by: '送料込み（出品者負担）'},
+  ]
+
+  include ActiveHash::Associations
+  has_many :items
 end
