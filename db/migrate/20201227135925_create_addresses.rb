3d01d0ea697d208dab7eb2105674185c3db4ccf9
Tomohiro4196 <tomo4196.w@gmail.com>
2020-12-27 23:05:09 +0900

    orderテーブルの作成

---
+++ db/migrate/20201227135925_create_addresses.rb
@@ -0,0 +1,14 @@
+class CreateAddresses < ActiveRecord::Migration[6.0]
+  def change
+    create_table :addresses do |t|
+      t.integer     :order_id,      null: false, foreign_key: true
+      t.integer     :phone_number,  null: false
+      t.integer     :prefecture_id, null: false
+      t.string      :city,          null: false
+      t.string      :town_block,    null: false
+      t.string      :building_name
+      t.integer     :zip_code,      null: false
+      t.timestamps
+    end
+  end
+end
