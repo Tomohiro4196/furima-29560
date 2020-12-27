e14956a375a34b74819be5ac36c24c5d818759f1
Tomohiro4196 <tomo4196.w@gmail.com>
2020-12-27 15:22:44 +0900

    単体テストコードの記述

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,14 @@
 FactoryBot.define do
   factory :item do
-    
+    item_name               { Faker::Lorem::words}
+    description             { Faker::Lorem::sentence}
+    category_id             { rand(2..11) }
+    condition_id            { rand(2..7) }
+    days_ship_id            { rand(2..4) }
+    prefecture_id           { rand(2..48) }
+    shipping_fee_id         { rand(2..3) }
+    price                   { rand(300..9999999) }
+
+    association :user
   end
 end
