505e8e0557370d54c088e3d552cc6244affb6fb1
Tomohiro4196 <tomo4196.w@gmail.com>
2021-01-24 12:22:22 +0900

    set_itemの修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :set_item, only: [:show, :edit, :update]
+  before_action :set_item, only: [:show, :edit, :update, :destroy]
   before_action :authenticate_user!, except: [:index, :show]
   before_action :check_user, only: [:edit]
 
@@ -35,7 +35,6 @@ class ItemsController < ApplicationController
   end
 
   def destroy
-    @item = Item.find(params[:id])
     if current_user.id == @item.user_id
       @item.destroy
       redirect_to root_path
