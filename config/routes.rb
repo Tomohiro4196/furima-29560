3b9b9e0c7fa36f6e346bc755c202cb8e56aef770
Tomohiro4196 <tomo4196.w@gmail.com>
2021-01-24 16:47:36 +0900

    ルーティングの修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "items#index"
-  resources :items, except: [:index] do
-    resource :order, only: [:index, :create]
+  resources :items do
+    resources :orders, only: [:index, :create]
   end
 end
