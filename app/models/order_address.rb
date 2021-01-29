2ae29830f4a7c04593518b4df80d85a5a9cac371
Tomohiro4196 <tomo4196.w@gmail.com>
2021-01-30 01:39:19 +0900

    バリデーションの修正

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -1,20 +1,19 @@
-# class OrderAddress
-#   include ActiveModel::Model
-#   attr_accessor :phone_number, :prefecture_id, :city, :town_block, :building_name, :zip_code
+class OrderAddress
+  include ActiveModel::Model
+  attr_accessor :phone_number, :prefecture_id, :city, :town_block, :building_name, :zip_code, :item_id, :buyer_id
 
-#   with_options presence: true do
-#     validates :phone_number, {with: /\A[0-9]{3}\z/, message:'is not valid'}
-#     validates :city
-#     validates :town_block
-#     validates :building_name
-#     validates :zip_code, { with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)" }
-#   end
+  with_options presence: true do
+    validates :phone_number, format: {with: /\A[0-9]{11}\z/, message:'is not valid'}
+    validates :city
+    validates :town_block
+    validates :zip_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)" }
+  end
 
-#   validates :prefecture_id, numericality: {other_than: 0, message: "can't be blank"}
+  validates :prefecture_id, numericality: {other_than: 0, message: "can't be blank"}
 
-#   def save
-#     order = Order.create(item_id: item.id, user_id: user.id)
-#     Address.create(order_id: order.id, phone_number: phone_number, prefecture_id: prefecture_id, city: city, town_block: town_block, building_name: building_name, zip_code: zip_code )
-#   end
+  def save
+    order = Order.create(item_id: item_id, buyer_id: buyer_id)
+    Address.create(order_id: order.id, phone_number: phone_number, prefecture_id: prefecture_id, city: city, town_block: town_block, building_name: building_name, zip_code: zip_code)
+  end
 
-# end
+end
